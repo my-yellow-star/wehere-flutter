780198dc17b6735471bbefee9b9b532527515fe6
triphero-chan <c01034784796@gmail.com>
2023-01-22 23:22:06 +0900

    wip: temporary ui for provider test

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:provider/provider.dart';
+import 'package:wehere_client/injector.dart';
+import 'package:wehere_client/presentation/provider/member_provider.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await initializeDependencies();
+  final memberProvider = injector<MemberProvider>();
+  await memberProvider.getMember();
+  FlutterNativeSplash.remove();
+  runApp(ChangeNotifierProvider(
+    create: (context) => memberProvider,
+    child: MaterialApp(
+      home: Home(),
+    ),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    super.initState();
+    // WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+    //   Provider.of<MemberProvider>(context, listen: false).getMember();
+    // });
+  }
 
   @override
   Widget build(BuildContext context) {
-    throw Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('member'),
+        ),
+        body: Consumer<MemberProvider>(
+          builder: (context, value, child) {
+            if (value.isLoading) {
+              return Center(child: CircularProgressIndicator());
+            }
+            final member = value.member;
+            return Center(
+              child: Text(member == null ? 'null' : member.nickname),
+            );
+          },
+        ));
   }
 }
