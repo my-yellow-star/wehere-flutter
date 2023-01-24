8683388781723e576feac68ce727c328b996f599
triphero-chan <c01034784796@gmail.com>
2023-01-24 22:35:48 +0900

    refactor: inherit api provider

--- lib/presentation/provider/authentication_provider.dart
+++ lib/presentation/provider/authentication_provider.dart
@@ -1,35 +1,28 @@
-import 'package:dio/dio.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:wehere_client/core/params/oauth2_login.dart';
 import 'package:wehere_client/core/resources/data_state.dart';
 import 'package:wehere_client/domain/entities/authentication.dart';
 import 'package:wehere_client/domain/usecases/oauth2_login_usecase.dart';
+import 'package:wehere_client/presentation/provider/api_provider.dart';
 
-class AuthenticationProvider extends ChangeNotifier {
+class AuthenticationProvider extends ApiProvider {
   final OAuth2LoginUseCase _oAuth2LoginUseCase;
 
   AuthenticationProvider(this._oAuth2LoginUseCase);
 
-  bool _isLoading = false;
   Authentication? _authentication;
-  DioError? _error;
-
-  bool get isLoading => _isLoading;
 
   Authentication? get authentication => _authentication;
 
-  DioError? get error => _error;
-
   Future<void> login(OAuth2LoginParams params) async {
-    _isLoading = true;
+    isLoading = true;
     notifyListeners();
     final response = await _oAuth2LoginUseCase(params);
     if (response is DataSuccess) {
       _authentication = response.data;
     } else {
-      _error = response.error;
+      error = response.error;
     }
-    _isLoading = false;
+    isLoading = false;
     notifyListeners();
   }
 }
--- lib/presentation/provider/member_provider.dart
+++ lib/presentation/provider/member_provider.dart
@@ -1,35 +1,28 @@
-import 'package:dio/dio.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:wehere_client/core/resources/data_state.dart';
 import 'package:wehere_client/domain/entities/member.dart';
 import 'package:wehere_client/domain/usecases/get_profile_usecase.dart';
+import 'package:wehere_client/presentation/provider/api_provider.dart';
 
-class MemberProvider extends ChangeNotifier {
+class MemberProvider extends ApiProvider {
   final GetProfileUseCase _getProfileUseCase;
 
   MemberProvider(this._getProfileUseCase);
 
-  bool _isLoading = false;
   Member? _member;
-  DioError? _error;
-
-  bool get isLoading => _isLoading;
 
   Member? get member => _member;
 
-  DioError? get error => _error;
-
   Future<void> getMember() async {
-    _isLoading = true;
+    isLoading = true;
     notifyListeners();
 
     final response = await _getProfileUseCase();
     if (response is DataSuccess) {
       _member = response.data;
     } else {
-      _error = response.error;
+      error = response.error;
     }
-    _isLoading = false;
+    isLoading = false;
     notifyListeners();
   }
 }
