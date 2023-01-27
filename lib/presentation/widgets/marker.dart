436b9afb25214d3b3cf9ba3e060f7c3589677d22
triphero-chan <c01034784796@gmail.com>
2023-01-28 08:31:12 +0900

    style: map marker custom style

--- lib/presentation/widgets/marker.dart
+++ lib/presentation/widgets/marker.dart
@@ -4,14 +4,53 @@ import 'package:wehere_client/presentation/widgets/profile_image.dart';
 
 class IMarker extends StatelessWidget {
   final NostalgiaSummary item;
+  static const double circleSize = 50;
+  static const double circleBorder = 4;
+  static const double triangleWidth = 12;
+  static const color = Colors.black;
 
   const IMarker({super.key, required this.item});
 
   @override
   Widget build(BuildContext context) {
-    return ProfileImage(
-      size: 36,
-      url: item.thumbnail,
+    return Stack(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(circleSize),
+              border: Border.all(color: color, width: circleBorder)),
+          child: ProfileImage(
+            size: circleSize - (circleBorder * 2),
+            url: item.thumbnail,
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(
+              top: circleSize - 1, left: (circleSize - triangleWidth) / 2),
+          child: ClipPath(
+            clipper: TriangleClipper(),
+            child: Container(
+              color: color,
+              width: triangleWidth,
+              height: triangleWidth / 2,
+            ),
+          ),
+        )
+      ],
     );
   }
+}
+
+class TriangleClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final path = Path();
+    path.lineTo(size.width, 0.0);
+    path.lineTo(size.width / 2, size.height);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(TriangleClipper oldClipper) => false;
 }
