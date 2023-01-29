5285c7912fd4049e352ce17b813f43824cb840c1
triphero-chan <c01034784796@gmail.com>
2023-01-29 23:17:40 +0900

    style: background image

--- lib/presentation/widgets/background_image.dart
+++ lib/presentation/widgets/background_image.dart
@@ -3,18 +3,27 @@ import 'package:flutter/material.dart';
 import '../../core/resources/constant.dart';
 
 class BackgroundImage extends StatelessWidget {
-  const BackgroundImage({super.key});
+  final String? url;
+
+  const BackgroundImage({super.key, this.url});
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Image.asset(
-          '${Constant.image}/airplane_window.jpg',
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          fit: BoxFit.cover,
-        ),
+        url == null
+            ? Image.asset(
+                '${Constant.image}/airplane_window.jpg',
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                fit: BoxFit.cover,
+              )
+            : Image.network(
+                url!,
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                fit: BoxFit.cover,
+              ),
         Container(
           color: ColorTheme.primary.withOpacity(0.8),
         ),
