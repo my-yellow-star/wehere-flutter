817334957ba0e0594ccf0527bc83c70992121bbe
triphero-chan <c01034784796@gmail.com>
2023-02-06 23:59:43 +0900

    chore: navigation on click item

--- lib/presentation/widgets/nostalgia_list_card.dart
+++ lib/presentation/widgets/nostalgia_list_card.dart
@@ -11,59 +11,64 @@ class NostalgiaListCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(left: 24, right: 24),
-      padding: EdgeInsets.only(left: 8, right: 16, top: 10, bottom: 10),
-      decoration: BoxDecoration(
-          color: Colors.black,
-          boxShadow: const [BoxShadow(blurRadius: 8)],
-          borderRadius: BorderRadius.all(Radius.circular(24))),
-      height: 100,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.all(Radius.circular(16)),
-            child: Image.network(
-              item.thumbnail!,
-              width: 80,
-              height: 80,
-              fit: BoxFit.cover,
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, 'nostalgia-detail', arguments: item.id);
+      },
+      child: Container(
+        margin: EdgeInsets.only(left: 24, right: 24),
+        padding: EdgeInsets.only(left: 8, right: 16, top: 10, bottom: 10),
+        decoration: BoxDecoration(
+            color: Colors.black,
+            boxShadow: const [BoxShadow(blurRadius: 8)],
+            borderRadius: BorderRadius.all(Radius.circular(24))),
+        height: 100,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(16)),
+              child: Image.network(
+                item.thumbnail!,
+                width: 80,
+                height: 80,
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          Container(width: 12),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                IText(item.title, weight: FontWeight.bold),
-                Container(height: 4),
-                IText(item.description, size: FontSize.small),
-                Expanded(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Row(
-                        children: [
-                          Icon(Icons.place_outlined,
-                              color: Colors.white, size: 16),
-                          IText(item.distance?.parseDistance() ?? '???',
-                              size: FontSize.small),
-                        ],
-                      ),
-                      IText(
-                        item.createdAt.parseDate(),
-                        size: FontSize.small,
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          )
-        ],
+            Container(width: 12),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  IText(item.title, weight: FontWeight.bold),
+                  Container(height: 4),
+                  IText(item.description, size: FontSize.small),
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.place_outlined,
+                                color: Colors.white, size: 16),
+                            IText(item.distance?.parseDistance() ?? '???',
+                                size: FontSize.small),
+                          ],
+                        ),
+                        IText(
+                          item.createdAt.parseDate(),
+                          size: FontSize.small,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
