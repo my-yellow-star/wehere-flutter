a5638b7eed710b96e3d93b21cf6ee52b594f8871
triphero-chan <c01034784796@gmail.com>
2023-02-06 21:08:54 +0900

    feat: add named routes

--- lib/presentation/routes.dart
+++ lib/presentation/routes.dart
@@ -1,6 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:wehere_client/presentation/screens/create_nostalgia_screen.dart';
+import 'package:wehere_client/presentation/screens/home_screen.dart';
+import 'package:wehere_client/presentation/screens/login_screen.dart';
+import 'package:wehere_client/presentation/screens/main_screen.dart';
+import 'package:wehere_client/presentation/screens/map_screen.dart';
+import 'package:wehere_client/presentation/screens/mypage_screen.dart';
+import 'package:wehere_client/presentation/screens/nostalgia_detail_screen.dart';
+import 'package:wehere_client/presentation/screens/permission_screen.dart';
 
 class Routes {
+  static Map<String, Widget Function(BuildContext)> map = {
+    'permission': (_) => PermissionScreen(),
+    'login': (_) => LoginScreen(),
+    'main': (_) => MainScreen(),
+    'home': (_) => HomeScreen(),
+    'map': (_) => MapScreen(),
+    'my-page': (_) => MyPageScreen(),
+    'create-nostalgia': (_) => CreateNostalgiaScreen(),
+    'nostalgia-detail': (_) => NostalgiaDetailScreen()
+  };
+
   static void push(BuildContext context, Widget screen) {
     Navigator.push(context, _noAnimation(screen));
   }
