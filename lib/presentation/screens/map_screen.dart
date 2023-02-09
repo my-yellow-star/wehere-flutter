6c0c5efd9396a60968c4007049b9bc0ed7e88c10
triphero-chan <c01034784796@gmail.com>
2023-02-09 23:06:48 +0900

    fix: replace auto api call to click to refresh button

--- lib/presentation/screens/map_screen.dart
+++ lib/presentation/screens/map_screen.dart
@@ -11,6 +11,7 @@ import 'package:wehere_client/domain/entities/location.dart';
 import 'package:wehere_client/domain/entities/nostalgia_summary.dart';
 import 'package:wehere_client/presentation/providers/location_provider.dart';
 import 'package:wehere_client/presentation/providers/nostalgia_list_provider.dart';
+import 'package:wehere_client/presentation/widgets/button.dart';
 import 'package:wehere_client/presentation/widgets/marker.dart';
 import 'package:wehere_client/presentation/widgets/marker_generator.dart';
 import 'package:wehere_client/presentation/widgets/mixin.dart';
@@ -27,42 +28,39 @@ class MapScreen extends StatefulWidget {
 class _MapScreenState extends State<MapScreen> with AfterLayoutMixin {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
-  List<NostalgiaSummary> nostalgiaList = [];
-  List<Uint8List> bitmaps = [];
-  NostalgiaSummary? tappedItem;
-  Set<Circle> circles = {};
-  double zoom = 12;
-  late Location location;
+  Set<Marker> _markers = {};
+  NostalgiaSummary? _tappedItem;
+  Set<Circle> _circles = {};
+  double _zoom = 12;
+  late Location _location;
+  bool _shouldRefresh = false;
 
   @override
   void initState() {
     super.initState();
     final locationProvider = context.read<LocationProvider>();
     locationProvider.getLocation();
-    location = context.read<LocationProvider>().location!;
+    _location = context.read<LocationProvider>().location!;
   }
 
   @override
   void afterFirstLayout(BuildContext context) {
-    _generateMarker(context);
+    _generateMarker();
   }
 
-  void _generateMarker(BuildContext context) {
+  void _generateMarker() {
     final nostalgia = context.read<NostalgiaListProvider>();
     nostalgia.initialize();
     nostalgia
         .loadList(
             condition: NostalgiaCondition.around,
-            latitude: location.latitude,
-            longitude: location.longitude,
+            latitude: _location.latitude,
+            longitude: _location.longitude,
             maxDistance: _maxDistance(context))
         .then((_) {
       MarkerGenerator(nostalgia.items.map((e) => IMarker(item: e)).toList(),
           (bitmaps) {
-        setState(() {
-          this.bitmaps = bitmaps;
-          nostalgiaList = nostalgia.items;
-        });
+        _mapBitmapsToMarkers(bitmaps, nostalgia.items);
       }).addOverlay(context);
     });
   }
@@ -71,54 +69,56 @@ class _MapScreenState extends State<MapScreen> with AfterLayoutMixin {
       _metersPerPx() * MediaQuery.of(context).size.height / 2;
 
   _metersPerPx() =>
-      156543.03392 * cos(location.latitude * pi / 180) / pow(2, zoom);
+      156543.03392 * cos(_location.latitude * pi / 180) / pow(2, _zoom);
 
-  Set<Marker> _mapBitmapsToMarkers() {
-    if (nostalgiaList.isEmpty) return {};
-    return bitmaps
-        .asMap()
-        .entries
-        .map((entry) => Marker(
-            consumeTapEvents: true,
-            markerId: MarkerId(nostalgiaList[entry.key].id),
-            position: _getPosition(nostalgiaList[entry.key]),
-            onTap: () {
-              setState(() {
-                tappedItem = nostalgiaList[entry.key];
-                circles = {_circle(nostalgiaList[entry.key])};
-              });
-            },
-            icon: BitmapDescriptor.fromBytes(entry.value)))
-        .toSet();
+  _mapBitmapsToMarkers(List<Uint8List> bitmaps, List<NostalgiaSummary> items) {
+    if (items.isEmpty) return;
+    setState(() {
+      _shouldRefresh = false;
+      _markers = bitmaps
+          .asMap()
+          .entries
+          .map((entry) => Marker(
+              consumeTapEvents: true,
+              markerId: MarkerId(items[entry.key].id),
+              position: _getPosition(items[entry.key]),
+              onTap: () {
+                setState(() {
+                  _tappedItem = items[entry.key];
+                  _circles = {_circle(items[entry.key])};
+                });
+              },
+              icon: BitmapDescriptor.fromBytes(entry.value)))
+          .toSet();
+    });
   }
 
   CameraPosition _currentPosition(BuildContext context) {
     return CameraPosition(
-        target: LatLng(location.latitude, location.longitude), zoom: zoom);
+        target: LatLng(_location.latitude, _location.longitude), zoom: _zoom);
   }
 
   LatLng _getPosition(NostalgiaSummary item) =>
       LatLng(item.location.latitude, item.location.longitude);
 
-  void _onCameraIdle(BuildContext context) {
-    _generateMarker(context);
-  }
+  void _onCameraIdle(BuildContext context) {}
 
   void _onCameraMove(CameraPosition position) {
     setState(() {
-      location = Location(position.target.latitude, position.target.longitude);
-      zoom = position.zoom;
+      _location = Location(position.target.latitude, position.target.longitude);
+      _zoom = position.zoom;
+      _shouldRefresh = true;
     });
     _metersPerPx();
   }
 
   void _onMapTapped(_) {
-    if (tappedItem == null) {
+    if (_tappedItem == null) {
       return;
     }
     setState(() {
-      tappedItem = null;
-      circles = {};
+      _tappedItem = null;
+      _circles = {};
     });
   }
 
@@ -145,7 +145,7 @@ class _MapScreenState extends State<MapScreen> with AfterLayoutMixin {
             initialCameraPosition: _currentPosition(context),
             myLocationEnabled: true,
             myLocationButtonEnabled: false,
-            markers: _mapBitmapsToMarkers(),
+            markers: _markers,
             onTap: _onMapTapped,
             onMapCreated: (GoogleMapController controller) {
               if (!_controller.isCompleted) {
@@ -156,37 +156,56 @@ class _MapScreenState extends State<MapScreen> with AfterLayoutMixin {
               _onCameraIdle(context);
             },
             onCameraMove: _onCameraMove,
-            circles: circles,
+            circles: _circles,
           ),
         ),
-        tappedItem == null
+        _tappedItem == null
             ? Container()
             : Align(
                 alignment: Alignment.bottomCenter,
-                child: NostalgiaMapCard(item: tappedItem!)),
+                child: NostalgiaMapCard(item: _tappedItem!)),
         SafeArea(
-            child: Container(
-          margin: EdgeInsets.all(16),
-          padding: EdgeInsets.fromLTRB(24, 6, 24, 6),
-          decoration: BoxDecoration(
-              color: Colors.black,
-              borderRadius: BorderRadius.circular(100),
-              boxShadow: [
-                BoxShadow(color: Colors.black.withOpacity(0.5), blurRadius: 8)
-              ]),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              IText(
-                '근처 추억 ',
-                size: FontSize.small,
+            child: Row(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.all(16),
+              padding: EdgeInsets.fromLTRB(24, 6, 24, 6),
+              decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(100),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black.withOpacity(0.5), blurRadius: 8)
+                  ]),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IText(
+                    '근처 추억 ',
+                    size: FontSize.small,
+                  ),
+                  IText(
+                    '${nostalgia.total}',
+                    weight: FontWeight.bold,
+                  ),
+                ],
               ),
-              IText(
-                '${nostalgia.total}',
-                weight: FontWeight.bold,
-              ),
-            ],
-          ),
+            ),
+            _shouldRefresh
+                ? RoundButton(
+                    icon: Icons.refresh,
+                    onPress: _generateMarker,
+                    backgroundOpacity: 1,
+                    backgroundColor: Colors.white,
+                    color: Colors.blue,
+                    shadowColor: Colors.blue,
+                  )
+                : Container(
+                    height: 0,
+                  )
+          ],
         ))
       ],
     );
